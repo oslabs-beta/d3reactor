4d585b43b63b43aa96cdf9e9e386af778f9aeaf1
Robert Crocker <robert@vizsimply.com>
2021-11-15 10:24:02 -0600

    Removed scatter.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import "./App.css"
 import LineChart from "./charts/LineChart/LineChart"
-import ScatterPlot from "./charts/ScatterPlot/ScatterPlot"
 import penguins from "./data/penguins.json"
 import portfolio from "./data/portfolio.json"
 
@@ -21,22 +20,6 @@ function App() {
         yAxis="left"
         xAxisLabel="Date"
         yAxisLabel="Value"
-      />
-      <ScatterPlot
-        data={portfolio}
-        // data={penguins}
-        height="100%"
-        width="100%"
-        xDataProp={{ key: "date", dataType: "date" }}
-        // xDataProp={{ key: "flipper_length_mm", dataType: "number" }}
-        yDataProp={{ key: "value", dataType: "number" }}
-        // yDataProp={{ key: "body_mass_g", dataType: "number" }}
-        xAxis="bottom"
-        yAxis="left"
-        xGrid={true}
-        yGrid={true}
-        xAxisLabel="Flipper, mm"
-        yAxisLabel="Body Mass, g"
       />
     </div>
   )
