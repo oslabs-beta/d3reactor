71d8e66d11390ce0e50fa14dd638339826c13149
Robert Crocker <robert@vizsimply.com>
2021-11-14 21:08:13 -0600

    Cleaned up the data type handling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 /* eslint-disable no-unused-vars */
-import './App.css';
-import LineChart from './charts/LineChart/LineChart';
-import penguins from './data/penguins.json'
-import portfolio from './data/portfolio.json'
-
+import "./App.css"
+import LineChart from "./charts/LineChart/LineChart"
+import penguins from "./data/penguins.json"
+import portfolio from "./data/portfolio.json"
 
 function App() {
-  console.log(portfolio)
   return (
     <div className="App">
-            <LineChart
-                // data={penguins}
-                data={portfolio}
-                height="100%"
-                width="100%" 
-                // xDataProp ={{key: 'body_mass_g', type: 'number'}}
-                xDataProp ={{key: 'date', type: 'date'}}
-                // yDataProp ={{key: 'flipper_length_mm', type: 'number'}}
-                yDataProp ={{key: 'value', type: 'number'}}
-                xAxis='bottom'
-                yAxis='left'
-                xAxisLabel = 'Date'
-                yAxisLabel = 'Value'
-            />
-     </div>
-  );
+      <LineChart
+        // data={penguins}
+        data={portfolio}
+        height="100%"
+        width="100%"
+        // xDataProp={{ key: "body_mass_g", dataType: "number" }}
+        xDataProp={{ key: "date", dataType: "date" }}
+        // yDataProp={{ key: "flipper_length_mm", dataType: "number" }}
+        yDataProp={{ key: "value", dataType: "number" }}
+        xAxis="bottom"
+        yAxis="left"
+        xAxisLabel="Date"
+        yAxisLabel="Value"
+      />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/charts/LineChart/LineChartBody.tsx
+++ src/charts/LineChart/LineChartBody.tsx
@@ -1,105 +1,134 @@
 /** LineChart.js */
-import React, {useMemo} from 'react';
-import * as d3 from "d3";
-import styled from "styled-components";
-import Axis from "../../components/Axis";
-import {Props} from '../../../types'
-import {getXAxisCoordinates, getYAxisCoordinates, getMargins} from '../../utils'
+import React, { useMemo } from "react"
+import * as d3 from "d3"
+import styled from "styled-components"
+import Axis from "../../components/Axis"
+import { Props } from "../../../types"
+import {
+  getXAxisCoordinates,
+  getYAxisCoordinates,
+  getMargins,
+} from "../../utils"
 
 const Path = styled.path`
   fill: none;
   stroke: black;
   stroke-width: 2px;
-`;
+`
 
-type AccessorFunc = (d: any) => number | Date;
-type ScaleFunc = d3.ScaleLinear<number, number, never> | d3.ScaleTime<number, number, never>;
+type AccessorFunc = (d: any) => number | Date
+type Domain = number | Date | undefined
+type ScaleFunc =
+  | d3.ScaleLinear<number, number, never>
+  | d3.ScaleTime<number, number, never>
 
 const LineChartBody = ({
-                         data,
-                         height,
-                         width,
-                         xDataProp,
-                         yDataProp,
-                         xAxis,
-                         yAxis,
-                         xAxisLabel,
-                         yAxisLabel
-                       }: Props<number>): JSX.Element => {
-  const margin = useMemo(() =>  getMargins(xAxis, yAxis, xAxisLabel, yAxisLabel), [xAxis, yAxis, xAxisLabel, yAxisLabel])
+  data,
+  height,
+  width,
+  xDataProp,
+  yDataProp,
+  xAxis,
+  yAxis,
+  xAxisLabel,
+  yAxisLabel,
+}: Props<number>): JSX.Element => {
+  const margin = useMemo(
+    () => getMargins(xAxis, yAxis, xAxisLabel, yAxisLabel),
+    [xAxis, yAxis, xAxisLabel, yAxisLabel]
+  )
 
-  const {
-    xAxisX,
-    xAxisY
-  } = useMemo(() => getXAxisCoordinates(xAxis, height, margin), [height, xAxis, margin])
+  const { xAxisX, xAxisY } = useMemo(
+    () => getXAxisCoordinates(xAxis, height, margin),
+    [height, xAxis, margin]
+  )
 
-  const {
-    yAxisX,
-    yAxisY
-  } = useMemo(() => getYAxisCoordinates(yAxis, width, margin), [width, yAxis, margin])
+  const { yAxisX, yAxisY } = useMemo(
+    () => getYAxisCoordinates(yAxis, width, margin),
+    [width, yAxis, margin]
+  )
 
-  const translate = `translate(${margin.left}, ${margin.top})`;
+  const translate = `translate(${margin.left}, ${margin.top})`
 
-  let xAccessor:AccessorFunc = (d) => d[xDataProp.key];
-  let xScale:ScaleFunc = d3
-    .scaleLinear()
-    // @ts-ignore
-    .domain(d3.extent(data, xAccessor))
-    .range([0, width - margin.right - margin.left]);
-
-  switch (xDataProp.type) {
-    case ('date'):
-      xAccessor = (d) => new Date(d[xDataProp.key]);
+  let xScale: ScaleFunc, xAccessor: AccessorFunc, xMin: Domain, xMax: Domain
+  switch (xDataProp.dataType) {
+    case "number":
+      xAccessor = (d) => d[xDataProp.key]
+      xMin = d3.extent(data, xAccessor)[0]
+      xMax = d3.extent(data, xAccessor)[1]
+      xScale = d3
+        .scaleLinear()
+        .domain([xMin ?? 0, xMax ?? 0])
+        .range([0, width - margin.right - margin.left])
+      break
+    case "date":
+      xAccessor = (d) => new Date(d[xDataProp.key])
+      xMin = d3.extent(data, xAccessor)[0]
+      xMax = d3.extent(data, xAccessor)[1]
       xScale = d3
         .scaleTime()
-        // @ts-ignore
-        .domain(d3.extent(data, xAccessor))
-        .range([0, width - margin.right - margin.left]);
-      break;
-    case ('string'): // TODO Example of how we could add other types
-      //
-      //
-      break;
+        .domain([xMin ?? 0, xMax ?? 0])
+        .range([0, width - margin.right - margin.left])
+      break
   }
 
-  let yAccessor:AccessorFunc = (d) => d[yDataProp.key];
-  let yScale:ScaleFunc = d3
-    .scaleLinear()
-    // @ts-ignore
-    .domain(d3.extent(data, yAccessor))
-    .rangeRound([height - margin.bottom - margin.top, 0]);
-
-  switch (yDataProp.type) {
-    case ('date'):
-      yAccessor = (d) => new Date(d[yDataProp.key]);
+  let yScale: ScaleFunc, yAccessor: AccessorFunc, yMin: Domain, yMax: Domain
+  switch (yDataProp.dataType) {
+    case "number":
+      yAccessor = (d) => d[yDataProp.key]
+      yMin = d3.extent(data, yAccessor)[0]
+      yMax = d3.extent(data, yAccessor)[1]
+      console.log("Min and max ", yMin, yMax)
+      yScale = d3
+        .scaleLinear()
+        .domain([yMin ?? 0, yMax ?? 0])
+        .range([height - margin.top - margin.bottom, 0])
+      break
+    case "date":
+      yAccessor = (d) => new Date(d[yDataProp.key])
+      yMin = d3.extent(data, yAccessor)[0]
+      yMax = d3.extent(data, yAccessor)[1]
       yScale = d3
         .scaleTime()
-        // @ts-ignore
-        .domain(d3.extent(data, yAccessor))
-        .range([height - margin.bottom - margin.top, 0]);
-      break;
-    case ('string'): // TODO Example of how we could add other types
-      //
-      //
-      break;
+        .domain([yMin ?? 0, yMax ?? 0])
+        .range([height - margin.top - margin.bottom, 0])
+      break
   }
 
-
-  // @ts-ignore
-  const line:any = d3
-  // @ts-ignore
+  const line: any = d3
     .line()
-  // @ts-ignore
     .x((d) => xScale(xAccessor(d)))
-    .y((d) => yScale(yAccessor(d)));
+    .y((d) => yScale(yAccessor(d)))
 
   return (
     <g transform={translate}>
       <Path className="line" d={line(data)} />
-      {yAxis && <Axis x={yAxisX} y={yAxisY} height={height} width={width} margin={margin} scale={yScale} type={yAxis} label={yAxisLabel}/>}
-      {xAxis && <Axis x={xAxisX} y={xAxisY} height={height} width={width} margin={margin} scale={xScale} type={xAxis} label={xAxisLabel}/>}
+      {yAxis && (
+        <Axis
+          x={yAxisX}
+          y={yAxisY}
+          height={height}
+          width={width}
+          margin={margin}
+          scale={yScale}
+          type={yAxis}
+          label={yAxisLabel}
+        />
+      )}
+      {xAxis && (
+        <Axis
+          x={xAxisX}
+          y={xAxisY}
+          height={height}
+          width={width}
+          margin={margin}
+          scale={xScale}
+          type={xAxis}
+          label={xAxisLabel}
+        />
+      )}
     </g>
-  );
-};
+  )
+}
 
-export default LineChartBody;
+export default LineChartBody
