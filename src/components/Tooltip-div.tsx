363d850aade268edb33b7cc4dd3d4b3826248ab2
hi2dmitri <idmytro@yahoo.com>
2022-01-09 12:40:42 -0500

    updated style of tooltip

--- src/components/Tooltip-div.tsx
+++ src/components/Tooltip-div.tsx
@@ -14,24 +14,25 @@ const TooltipDiv = ({
   const tooltipStyle: React.CSSProperties | undefined = {
     left: x,
     top: y,
-    transform: "translateX(-50%, -50%)",
+    transform: "translate(-50%, -150%)",
     position: "absolute",
     boxShadow: "0px 5px 15px 0px rgba(0,0,0,0.3)",
     zIndex: "9999",
-    pointerEvents:'none'
+    pointerEvents:'none',
+    background: "white",
+    margin: "4px 4px",
+    padding: "0.2em .5em",
+    borderRadius: "4px",
+    color: "#1e2023",
+    transition: "all 0.1s ease-out",
   }
 
   const contentStyle: React.CSSProperties | undefined = {
     maxWidth: "450px",
     position: "absolute",
-    transform: "translate(-50%, -75px)",
-    background: "white",
+    transform: "translate(-50%, -150%)",
     boxShadow: "0px 5px 15px 0px rgba(0,0,0,0.3)",
 
-    margin: "4px 4px",
-    padding: "0.2em .5em",
-    borderRadius: "4px",
-    color: "#1e2023",
     /* You can also use a fixed width and ommit the white-sapce. */
     // whiteSpace: "nowrap",
     // transform: "translate(calc( -50% + ${x}px), calc(-100% + ${y}px))",
@@ -39,50 +40,19 @@ const TooltipDiv = ({
     lineHeight: "1.5em",
     fontSize: "0.8em",
     zIndex: "9999",
-    transition: "all 0.1s ease-out",
   }
 
-  const triangleWrapperStyle: React.CSSProperties | undefined = {
-    position: "absolute",
-    width: "50px",
-    height: "25px",
-    transform: "translateY(-50%) rotate(45deg)",
-    overflow: "hidden",
-  }
 
-  const triangleStyle: React.CSSProperties | undefined = {
-    content: "",
-    position: "absolute",
-    width: "20px",
-    height: "20px",
-    background: "white",
-    // transform: "translateX(-50%, -50%) rotate(45deg)",
-    boxShadow: "1px 1px 20px 0px rgba(0,0,0,0.6)",
-  }
-
-  const circleStyle: React.CSSProperties | undefined = {
-    width: "2px",
-    height: "2px",
-    borderRadius: "50%",
-    position: "absolute",
-    transform: "translateX(-10px, -10px)",
-    backgroundColor: "black",
-  }
 
   const xTooltipText: string = `${xKey}: ${data.tooltipData[xKey as string]}`
   const yTooltipText: string = `${yKey}: ${data.tooltipData[yKey as string]}`
 
   return (
     <div style={tooltipStyle}>
-      <div style={contentStyle}>
         {xTooltipText}
         <br />
         {yTooltipText}
-      </div>
-      <div style={triangleWrapperStyle}>
-        <div style={triangleStyle}></div>
-      </div>
-      <div style={circleStyle}></div>
+
     </div>
   )
 }
