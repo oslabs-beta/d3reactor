67ecd2da5e219d213d8ce4482c960c3dddf67d3b
hi2dmitri <idmytro@yahoo.com>
2022-01-20 13:44:23 -0500

    prevented Listeningrect from rendering with negative width and height

--- src/components/__tests__/Circle.test.tsx
+++ src/components/__tests__/Circle.test.tsx
@@ -42,11 +42,6 @@ describe('Circle test', () => {
     expect(screen.getByTestId('d3reactor-circle')).toHaveAttribute('r', '4');
   });
 
-  test('Circle should have given radius', () => {
-    setup(circleProps);
-    expect(screen.getByTestId('d3reactor-circle')).toHaveAttribute('r', circleProps.r);
-  });
-
   test('Circle should have given cx attribute', () => {
     setup(circleProps);
     expect(screen.getByTestId('d3reactor-circle')).toHaveAttribute('cx', circleProps.cx.toString());
