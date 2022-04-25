7dd57950e91ac7af90874c848219792aa610f6e9
Robert Crocker <robert@vizsimply.com>
2022-04-25 11:49:21 -0400

    Needed to reset the App.js file.

--- src/App.tsx
+++ src/App.tsx
@@ -22,25 +22,79 @@ function App() {
   const [line, setLine] = useState(unemployment.slice(0, 60));
   const [scatter, setScatter] = useState(penguins.slice(30, 60));
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setPie(fruit.sort((a, b) => a.value - b.value));
-  //   }, 1000);
-  //   setTimeout(() => {
-  //     setBar(skinny_fruit.reverse());
-  //   }, 2000);
-  //   setTimeout(() => {
-  //     setArea(portfolio.slice(0, 60));
-  //   }, 4000);
-  //   setTimeout(() => {
-  //     setLine(unemployment);
-  //   }, 6000);
-  //   setTimeout(() => {
-  //     setScatter(penguins);
-  //   }, 8000);
-  // }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setPie(fruit.sort((a, b) => a.value - b.value));
+    }, 1000);
+    setTimeout(() => {
+      setBar(skinny_fruit.reverse());
+    }, 2000);
+    setTimeout(() => {
+      setArea(portfolio.slice(0, 60));
+    }, 4000);
+    setTimeout(() => {
+      setLine(unemployment);
+    }, 6000);
+    setTimeout(() => {
+      setScatter(penguins);
+    }, 8000);
+  }, []);
   return (
     <Container className="app">
+      <PieChart
+        theme="dark"
+        data={pie}
+        label="label"
+        value="value"
+        outerRadius={400}
+        pieLabel={true}
+      />
+      <BarChart
+        theme="light"
+        height="100%"
+        width="100%"
+        data={bar}
+        xKey="date"
+        yKey="value"
+        groupBy="fruit"
+        xAxis="bottom"
+        yAxis="right"
+        yGrid={true}
+        xAxisLabel="Date"
+        yAxisLabel="Value"
+        legend={'bottom'}
+        tooltipVisible={true}
+      />
+      <AreaChart
+        theme="light"
+        height="100%"
+        width="100%"
+        data={area}
+        xKey="date"
+        yKey="value"
+        xAxis="bottom"
+        yAxis="right"
+        yGrid={true}
+        xAxisLabel="Date"
+        yAxisLabel="Value"
+      />
+      <LineChart
+        theme="light"
+        height={'100%'}
+        width={'100%'}
+        data={line}
+        xKey="date"
+        xDataType="date"
+        groupBy="division"
+        yKey="unemployment"
+        xAxis="bottom"
+        yAxis="left"
+        yGrid={true}
+        xAxisLabel="Date"
+        yAxisLabel="Value"
+        legend={'right'}
+        legendLabel="Markets"
+      />
       <ScatterPlot
         theme="light"
         height="100%"
